0d9b1f6b685f06ad8cd2246881f525e316eb571b
that-jamal <varg.jamil@gmail.com>
2023-03-22 13:55:54 +0100

    add

--- Rakna-ut-alder/main.js
+++ Rakna-ut-alder/main.js
@@ -1,5 +1,5 @@
 let input = document.querySelector("input");
-//let answer = document.getElementById("answer")
+let answer = document.getElementById("answer")
 let objectDate = new Date();
 let d = objectDate.getDate();
 let m = objectDate.getMonth() + 1;
@@ -9,30 +9,41 @@ let day = document.getElementById("day")
 let month = document.getElementById("month")
 let year = document.getElementById("year")
 
+
+function daysInMonth(month) {
+    return new Date(2023, month, 0).getDate();
+    //daysInMonth(3) = 31
+}
 let nowD
 let nowM
 let nowY
 
-year.addEventListener("keypress", function (event) {
+boxb.addEventListener("keypress", function (event) {
     nowD = d - day.value
     nowM = m - month.value
     nowY = y - year.value
-
-    answer()
-
-})
-
-function answer() {
+    if (0 > nowD) {
+        nowD = nowD + daysInMonth(3)
+        nowM--
+        if (0 > nowM) {
+            nowM = nowM + 12
+            nowY--
+        }
+    }
     if (0 > nowM) {
-        nowM + 12
-        nowY - 1
-
+        nowM = nowM + 12
+        nowY--
+    }
+    if (31 < day.value) {
+        day.value = 31
+    }
+    if (12 < month.value) {
+        month.value = 12
+    }
+    if (2023 < year.value) {
+        year.value = 2023
     }
 
 
-    // console.log(nowD + "/" + nowM + "/" + nowY)
-    console.log(nowM)
-}
-day.value = 4
-year.value = 2005
-month.value = 4
+    answer.innerText = ("age = " + nowD + "D" + "/" + nowM + "M" + "/" + nowY + "Y")
+})
