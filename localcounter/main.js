e64a3121d6707b1d1c8c3f8cb5eb509c1696d44d
that-jamal <varg.jamil@gmail.com>
2023-05-05 09:46:57 +0200

    ai un

--- localcounter/main.js
+++ localcounter/main.js
@@ -1,11 +1,10 @@
 let plus = document.getElementById("+")
 let count = document.getElementById("=")
 let minus = document.getElementById("-")
-
+let number = localStorage.getItem("number");
 
 plus.addEventListener("click", function () {
     number++
-    console.log(number)
     update()
     localStorage.setItem("number", number);
     count.textContent = number;
@@ -13,7 +12,6 @@ plus.addEventListener("click", function () {
 
 minus.addEventListener("click", function () {
     number--
-    console.log(number)
     update()
     localStorage.setItem("number", number);
     count.textContent = number;
@@ -22,15 +20,5 @@ minus.addEventListener("click", function () {
 function update() {
     count.innerText = number
 }
-//-------------------------- AI
 
-// Retrieve the stored value from local storage
-let number = localStorage.getItem("number");
-
-// If no value is stored, set the default value to 1
-if (!number) {
-    number = 0;
-    localStorage.setItem("number", number);
-}
-
-count.textContent = number;
+update()
